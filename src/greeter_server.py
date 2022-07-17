f6e097b83d25fca4423c1cff2937f8777e4cbd87
zzlev <gpalektzja@gmail.com>
2022-07-17 17:04:17 +0900

    Add reflection to greeter_server.

--- src/greeter_server.py
+++ src/greeter_server.py
@@ -4,6 +4,7 @@ from concurrent import futures
 import logging
 
 import grpc
+from grpc_reflection.v1alpha import reflection
 import helloworld_pb2, helloworld_pb2_grpc
 
 
@@ -22,6 +23,11 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    SERVICE_NAMES = (
+        helloworld_pb2.DESCRIPTOR.services_by_name["Greeter"].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server=server)
     server.add_insecure_port("[::]:50051")
     server.start()
     server.wait_for_termination()
