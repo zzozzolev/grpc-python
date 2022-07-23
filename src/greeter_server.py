6e026003d29db61c35ccd9bdd7ae429c7529f138
zzlev <gpalektzja@gmail.com>
2022-07-23 19:22:52 +0900

    Add anonymous bad request handling.

--- src/greeter_server.py
+++ src/greeter_server.py
@@ -3,17 +3,25 @@
 from concurrent import futures
 import logging
 from typing import Iterable
-from urllib import response
 
 import grpc
+from google.protobuf import any_pb2
+from google.rpc import error_details_pb2, code_pb2, status_pb2
 from grpc_reflection.v1alpha import reflection
+from grpc_status import rpc_status
 import helloworld_pb2, helloworld_pb2_grpc
 
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
     STREAM_GREETING = ["one", "two", "three", "four"]
 
-    def SayHello(self, request, context):
+    def SayHello(
+        self, request: helloworld_pb2.HelloRequest, context: grpc.ServicerContext
+    ):
+        if request.name == "anonymous":
+            status = self._get_anonymous_status()
+            context.abort_with_status(rpc_status.to_status(status))
+
         is_welcome = True if request.name == "hyemi" else False
         return helloworld_pb2.HelloReply(
             message=f"Hello, {request.name}!", is_welcome=is_welcome
@@ -25,6 +33,23 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
         is_welcome = True if request.name == "hyemi" else False
         for greeting in self.STREAM_GREETING:
             yield helloworld_pb2.HelloReply(message=greeting, is_welcome=is_welcome)
+
+    def _get_anonymous_status(self):
+        detail = any_pb2.Any()
+        detail.Pack(
+            error_details_pb2.BadRequest(
+                field_violations=[
+                    error_details_pb2.BadRequest.FieldViolation(
+                        field="name", description="name 'anonymous' is invalid."
+                    )
+                ]
+            )
+        )
+        return status_pb2.Status(
+            code=code_pb2.INVALID_ARGUMENT,
+            message="Bad Request",
+            details=[detail],
+        )
 
 
 def serve():
